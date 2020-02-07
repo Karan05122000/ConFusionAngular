97fb334331ca25800b4eba1c987e99bcc04e9c6b
Karan Kangude <karankangude17@gmail.com>
2020-02-07 19:15:22 +0530

    Components part 1

---
+++ src/app/shared/dish.ts
@@ -0,0 +1,10 @@
+export class Dish{
+    id: string;
+    name: string;
+    description: string;
+    category: string;
+    featured: boolean;
+    image: string;
+    price: string;
+    label: string;
+};
